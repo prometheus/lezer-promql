bdc94ddc655fee83d05a05b34b65fd8fbb105667
Augustin Husson <husson.augustin@gmail.com>
2021-05-04 11:19:50 +0200

    introduced the optional dialects variables
    Signed-off-by: Augustin Husson <husson.augustin@gmail.com>

--- test/test-promql.js
+++ test/test-promql.js
@@ -12,6 +12,6 @@ for (let file of fs.readdirSync(caseDir)) {
   let name = /^[^\.]*/.exec(file)[0]
   describe(name, () => {
     for (let {name, run} of fileTests(fs.readFileSync(path.join(caseDir, file), "utf8"), file))
-      it(name, () => run(parser))
+      it(name, () => run(parser.configure({dialect:'variables'})))
   })
 }
